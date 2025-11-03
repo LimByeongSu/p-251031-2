48844bb743d9c655a282e299b464c09f945349c9
caketower <dlaqudtn1107@naver.com>
2025-11-03 10:15:51 +0900

    2단계 : build.gradle.kts에 옵션 추가(open을 자동으로 넣어주는)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
-    kotlin("jvm")
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -44,6 +45,12 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     implementation(kotlin("stdlib-jdk8"))
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
