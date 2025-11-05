507eede7a1a92aaa78bdfdee78a88c55fa34c674
caketower <dlaqudtn1107@naver.com>
2025-11-05 09:46:10 +0900

    17단계 : GlobalExceptionHandler 변환

--- backend/src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.kt
+++ backend/src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.kt
@@ -1,65 +1,56 @@
-package com.back.global.globalExceptionHandler;
+package com.back.global.globalExceptionHandler
 
-import com.back.global.exception.ServiceException;
-import com.back.global.rsData.RsData;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Comparator;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
+import com.back.global.exception.ServiceException
+import com.back.global.rsData.RsData
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(NoSuchElementException.class)
+class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException::class)
     @ResponseBody
-    public RsData<Void> handleException(NoSuchElementException e){
-        return new RsData<Void>(
-                "404-1",
-                "존재하지 않는 데이터입니다."
-        );
+    fun handleException(e: NoSuchElementException): RsData<Void> {
+        return RsData<Void>(
+            "404-1",
+            "존재하지 않는 데이터입니다."
+        )
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
     @ResponseBody
-    public RsData<Void> handleException(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult()
-                .getAllErrors()
-                .stream()
-                .filter(error -> error instanceof FieldError)
-                .map(error -> (FieldError) error)
-                .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
-                .sorted(Comparator.comparing(String::toString))
-                .collect(Collectors.joining("\n"));
+    fun handleException(e: MethodArgumentNotValidException): RsData<Void?> {
+        val message = e.bindingResult
+            .allErrors
+            .filterIsInstance<FieldError>()
+            .map { it.field + "-" + it.code + "-" + it.defaultMessage}
+            .sorted()
+            .joinToString("\n")
 
-        return new RsData<Void>(
-                "400-1",
-                message
-        );
+        return RsData(
+            "400-1",
+            message
+        )
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ExceptionHandler(HttpMessageNotReadableException::class)
     @ResponseBody
-    public RsData<Void> handleException(HttpMessageNotReadableException e) {
-        return new RsData<Void>(
-                "400-2",
-                "잘못된 형식의 요청 데이터입니다."
-        );
+    fun handleException(e: HttpMessageNotReadableException?): RsData<Void> {
+        return RsData<Void>(
+            "400-2",
+            "잘못된 형식의 요청 데이터입니다."
+        )
     }
 
-    @ExceptionHandler(ServiceException.class)
+    @ExceptionHandler(ServiceException::class)
     @ResponseBody
-    public RsData<Void> handleException(ServiceException e) {
-        return new RsData<Void>(
-                e.resultCode,
-                e.msg
-        );
+    fun handleException(e: ServiceException): RsData<Void> {
+        return RsData<Void>(
+            e.resultCode,
+            e.msg
+        )
     }
-
-
 }
